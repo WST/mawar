11553899543c7938308b7c650f161c105ed74caa
WST <ilya.averkov@gmail.com>
2009-10-24 09:20:21 +0000

    Поправка к базе данных, скоро буду делать ростеры

--- mawar.sql
+++ mawar.sql
@@ -15,10 +15,20 @@ CREATE TABLE spool (
 ) DEFAULT CHARACTER SET UTF8 COLLATE utf8_bin;
 
 -- Таблица закладок на MUC
+-- Временный костыль вместо XML-хранилища
 CREATE TABLE bookmarks (
 	id_bookmark INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, -- ID закладки
 	id_user INT UNSIGNED NOT NULL, -- ID владельца закладки
 	bookmark_name VARCHAR(40) NOT NULL, -- название закладки
 	bookmark_jid VARCHAR(255) NOT NULL, -- JID закладки
 	bookmark_nick VARCHAR(40) NOT NULL -- ник юзера
+) DEFAULT CHARACTER SET UTF8 COLLATE utf8_bin;
+
+CREATE TABLE roster (
+	id_contact INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id_user INT(11) UNSIGNED NOT NULL,
+	contact_nick VARCHAR(64) NOT NULL,
+	contact_group VARCHAR(64),
+	contact_subscription CHAR(1) NOT NULL,
+	contact_pending CHAR(1) NOT NULL
 ) DEFAULT CHARACTER SET UTF8 COLLATE utf8_bin;
