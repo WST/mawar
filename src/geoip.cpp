092319f209129b489994fa7c13fe8a11f3365b27
WST <ilya.averkov@gmail.com>
2010-03-08 13:54:33 +0000

    Поддержка GeoIP. Первый commit. Пока малоюзабельно.

---
+++ src/geoip.cpp
@@ -0,0 +1,19 @@
+
+#include "geoip.h"
+
+GeoIPValidator::GeoIPValidator() {
+	geoip = GeoIP_open("GeoIP.dat", GEOIP_STANDARD);
+}
+
+GeoIPValidator::~GeoIPValidator() {
+	delete geoip;
+}
+
+bool GeoIPValidator::checkGeoIP(const char *ip_addr) {
+	const char* code = GeoIP_country_code_by_addr(geoip, ip_addr);
+	if(code == "RU" || code == "US" || code == "ID") {
+		return true;
+	} else {
+		return false;
+	}
+}
