3db904abbc1a1e5f6786980e69f6a0ced8f1262c
WST <ilya.averkov@gmail.com>
2010-03-08 15:13:03 +0000

    Млин, случайно сделал битую ревизию. Поправка.

--- src/include/geoip.h
+++ src/include/geoip.h
@@ -1,3 +1,17 @@
+#ifndef MAWAR_GEOIP_H
+#define MAWAR_GEOIP_H
 
 #include <GeoIP.h>
 
+class GeoIPValidator
+{
+	public:
+		GeoIPValidator();
+		~GeoIPValidator();
+		bool checkGeoIP(const char *ip_addr);
+	
+	private:
+		GeoIP* geoip; // база GeoIP
+};
+
+#endif
